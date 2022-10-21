7c098ab33b228711e1dbe0841eb0ea7db6246af9
Michelle <michelle.johansson85@gmail.com>
2022-10-21 10:33:21 +0200

    Update Login.js justified content  changed button to justifyContent center of the screen and TextInput visuals for better visual appearance

--- navigation/screens/Login.js
+++ navigation/screens/Login.js
@@ -59,20 +59,11 @@ export default class Login extends Component {
     );
 
     }
-
  }
 
  const styles = StyleSheet.create({
-     SafeAreaViewcontainer: { flex: 1, width: '100%' },
-     button: {
-         alignItems: 'center',
-             justifyContent: 'center',
-             paddingVertical: 12,
-             paddingHorizontal: 32,
-             borderRadius: 4,
-             elevation: 3,
-             backgroundColor: 'black',
-     },
+     SafeAreaViewcontainer: { flex: 1, width: '100%' , justifyContent: 'center'},
+
      buttonsubmit: {
          width: '100%',
          flex: 1,
@@ -88,6 +79,11 @@ export default class Login extends Component {
         display: "flex",
      },
      textinput:{
-         borderWidth: 1,
+         borderWidth: 2,
+         marginLeft: "10%",
+         marginRight: "10%",
+         borderRadius: 10 ,
+         borderColor: '#dadee3',
+         marginBottom: 10,
      }
  });
