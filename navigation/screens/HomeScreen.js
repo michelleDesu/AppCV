62d5bd0f342f0c5d1252843fa0f5b4c6bc022df7
Michelle <michelle.johansson85@gmail.com>
2022-10-21 12:53:11 +0200

    Update HomeScreen.js forgot to commit the actual file earlier commit

--- navigation/screens/HomeScreen.js
+++ navigation/screens/HomeScreen.js
@@ -20,21 +20,38 @@ function HomeScreen({navigation}) {
 
     const source= require('./extras/images/my-icon.png');
   return (
-   <SafeAreaView >
-           <View>
-           <Section >
-           <Images imageSource={source}/>
-           </Section>
-                <Section title="Michelle Olin" >
-                   Mainly educated at Blekinge Institute of Technology where I
-                   studied game programming. During my worklife I have worked in
-                   various administrative positions where communication and a
-                   service-oriented mindset have been necessary to perform daily
-                   tasks, but also in areas involving programming and IT.
+    <SafeAreaView style={styles.SafeAreaViewcontainer}>
+        <ScrollView style={styles.scrollView}>
+            <View style={styles.bgColor}>
+                <Section >
+                <Images imageSource={source}/>
                 </Section>
-           </View>
-   </SafeAreaView>
-  );
+                    <Section title="Michelle Olin" >
+                    Mainly educated at Blekinge Institute of Technology where I
+                    studied game programming. During my worklife I have worked in
+                    various administrative positions where communication and a
+                    service-oriented mindset have been necessary to perform daily
+                    tasks, but also in areas involving programming and IT.
+                </Section>
+            </View>
+        </ScrollView>
+    </SafeAreaView>
+    );
 }
 
 export default HomeScreen;
+
+const styles = StyleSheet.create({
+    SafeAreaViewcontainer: { flex: 1, width: '100%' },
+
+   bgColor:{
+     backgroundColor: '#f2dec4',
+   },
+   sectionTitle:{
+    backgroundColor: '#3f4042',
+   },
+   scrollView: {
+        backgroundColor: '#f2dec4',
+      },
+ });
+
